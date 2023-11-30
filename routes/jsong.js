b5c9e1bfbfe322735bf807daa9bf1f51b2e37167
timothy <timothyjohan11@gmail.com>
2023-11-30 19:27:09 +0700

    Multer install n setup

--- routes/jsong.js
+++ routes/jsong.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const Jsong = require('../models/Jsong');
+const multer = require('multer');
 
+const storage = multer.diskStorage({
+  destination: function(req, file, cb){
+    return cb(null, "./images")
+  },
+  filename:function(req, file, cb){
+    return cb(null, `${Date.now()}_${file.originalname}`)
+  }
+})
+
+const upload = multer({storage})
 
 //Add new Jsong contestant
-router.post('/new', async (req, res) => {
+router.post('/new', upload.single('bukti'), async (req, res) => {
   const {nama_peserta, telp, nama_panggung, lagu, link} = req.body
+  const bukti = req.file
   const newJsong = {
     nama_peserta: nama_peserta,
     telp: telp,
