6a97fd666132410255b213c033f32b75c3c74fd1
1maginaryNumbers <104255911+1maginaryNumbers@users.noreply.github.com>
2023-12-06 14:42:00 +0700

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,102 @@
 const express = require("express");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
-const { loginAndRegister } = require("../controllers/controller");
+// const { loginAndRegister } = require("../controllers/controller");
+const JWT_KEY = "proyek2gatel";
 
-// router.get("/", async (req, res) => {
-//     const data = await User.find();
-//     res.send(data);
-// });
+router.post("/", async (req, res) => {
+    const { username, password } = req.body;
 
-router.get("/", loginAndRegister);
+    if (username && password) {
+        const user = await User.findOne({ username });
+        if (user) {
+            if (user.password === password) {
+                const token = jwt.sign(
+                    {
+                        username: user.username,
+                        password: user.password,
+                    },
+                    JWT_KEY,
+                    { expiresIn: "10s" }
+                );
+                user.updateOne({ api_key: token });
+
+                return res.status(200).json({
+                    body: {
+                        username,
+                        token,
+                    },
+                });
+            } else {
+                return res.status(400).json({ message: "Incorrect Password" });
+            }
+        } else {
+            return res.status(404).json({ message: "User not found" });
+        }
+    } else {
+        return res.status(400).json({ message: "Every field must be filled" });
+    }
+});
+
+router.get("/", async (req, res) => {
+    const { user } = req.body;
+    console.log(user);
+    let token = req.header("x-auth-token");
+    if (!req.header("x-auth-token")) {
+        return res.status(400).send("Authentication token is missing");
+    }
+    let userdata;
+    try {
+        userdata = jwt.verify(token, JWT_KEY);
+    } catch (err) {
+        return res.status(400).send("Invalid JWT Key");
+    }
+
+    let result = { userdata };
+
+    return res.status(201).send({
+        result,
+    });
+});
 
 module.exports = router;
+
+// const jwt = require("jsonwebtoken");
+// const User = require("../models/User");
+// const JWT_KEY = "proyek2gatel";
+
+// const loginAndRegister = async (req, res) => {
+//     const { username, password } = req.body;
+
+//     if (username && password) {
+//         const user = await User.findOne({ username });
+//         if (user) {
+//             if (user.password === password) {
+//                 const token = jwt.sign(
+//                     {
+//                         username: user.username,
+//                         password: user.password,
+//                     },
+//                     JWT_KEY,
+//                     { expiresIn: "1h" }
+//                 );
+
+//                 return res.status(200).json({
+//                     body: {
+//                         username,
+//                         token,
+//                     },
+//                 });
+//             } else {
+//                 return res.status(400).json({ message: "Incorrect Password" });
+//             }
+//         } else {
+//             return res.status(404).json({ message: "User not found" });
+//         }
+//     } else {
+//         return res.status(400).json({ message: "Every field must be filled" });
+//     }
+// };
+
+// module.exports = { loginAndRegister };
