a98cf8f4ef741a718e21ce12e63c5d25b0c722f0
lthreen <timothyjohan11@gmail.com>
2023-12-13 18:21:47 +0700

    Endpoint post photo

--- routes/gallery.js
+++ routes/gallery.js
@@ -12,55 +12,37 @@ const storage = multer.diskStorage({})
 
 const upload = multer({storage})
 
-//Add new Coswalk contestant
-// router.post('/new', upload.single('bukti'), async (req, res) => {
-//   const {nama_peserta, nama_panggung, instagram} = req.body
-//   const bukti = fs.readFileSync(req.file.path, { encoding: 'base64' });
-
-
-//   try {
-//     const imgurResponse = await axios.post(
-//       `https://api.imgur.com/3/image?client_id=${process.env.IMGUR_CLIENT_ID}`,
-//       {
-//         image: bukti,
-//       },
-//       {
-//         headers: {
-//           Authorization: `Bearer ${process.env.IMGUR_TOKEN}`,
-//         },
-
-//       }
-//       )
-//       let tempImg = imgurResponse.data.data.link
-      
-//       const newCoswalk = {
-//         nama_peserta: nama_peserta,
-//         nama_panggung:nama_panggung,
-//         instagram:instagram,
-//         img:tempImg,
-//         status:false
-//       }
-
-//     await Coswalk.create(newCoswalk)
-    
-//     return res.status(201).send(newCoswalk)
-//   } catch (error) {
-//     return res.status(500).send(error)
-//   }
-
-// })
-
-router.post('/', async (req, res) => {
+//Add new Photo contestant
+router.post('/new', upload.single('photo'), async (req, res) => {
+  const photo = fs.readFileSync(req.file.path, { encoding: 'base64' });
+  
   try {
-    const request = await Gallery.create({
-      img: 'hehehe'
-    })
-    return res.status(201).send(request)
+    const imgurResponse = await axios.post(
+      `https://api.imgur.com/3/image?client_id=${process.env.IMGUR_CLIENT_ID}`,
+      {
+        image: photo,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.IMGUR_TOKEN}`,
+        },
+        
+      }
+      )
+      let tempImg = imgurResponse.data.data.link
+      
+      const newphoto = {
+        img:tempImg
+      }
+
+    await Gallery.create(newphoto)
     
+    return res.status(201).send(newphoto)
   } catch (error) {
-    return res.status(500).send(error)
-    
+    return res.status(500).send(error.message)
   }
+
 })
+
 
 module.exports = router
