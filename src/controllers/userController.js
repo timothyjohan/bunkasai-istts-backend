a85056f36a0aad2daf38545fb9bed9d3f3b24496
NeutralDM <go.greg.a.gosal1234@gmail.com>
2025-03-30 18:50:36 +0700

    add name and phone_number property to user model, add name and phone to response

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,91 +2,96 @@ const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
 const loginUser = async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    // Regex untuk memvalidasi format email
-    // Penjelasan:
-    // ^ : Menandakan awal dari string.
-    // [^\s@]+ : Mencocokkan satu atau lebih karakter yang bukan spasi (\s) atau tanda @.
-    // @ : Karakter @ harus ada di posisi ini.
-    // [^\s@]+ : Mencocokkan satu atau lebih karakter yang bukan spasi atau tanda @ setelah karakter @.
-    // \. : Karakter titik (.) harus ada di posisi ini.
-    // [^\s@]+ : Mencocokkan satu atau lebih karakter yang bukan spasi atau tanda @ setelah karakter titik.
-    // $ : Menandakan akhir dari string.
-    // Regex ini memastikan bahwa alamat email memiliki format yang benar, yaitu bagian sebelum @, diikuti oleh @, diikuti oleh bagian setelah @, dan diakhiri dengan domain yang valid.
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-        return res.status(400).send({ message: "Email tidak valid!" });
-    }
+  // Regex untuk memvalidasi format email
+  // Penjelasan:
+  // ^ : Menandakan awal dari string.
+  // [^\s@]+ : Mencocokkan satu atau lebih karakter yang bukan spasi (\s) atau tanda @.
+  // @ : Karakter @ harus ada di posisi ini.
+  // [^\s@]+ : Mencocokkan satu atau lebih karakter yang bukan spasi atau tanda @ setelah karakter @.
+  // \. : Karakter titik (.) harus ada di posisi ini.
+  // [^\s@]+ : Mencocokkan satu atau lebih karakter yang bukan spasi atau tanda @ setelah karakter titik.
+  // $ : Menandakan akhir dari string.
+  // Regex ini memastikan bahwa alamat email memiliki format yang benar, yaitu bagian sebelum @, diikuti oleh @, diikuti oleh bagian setelah @, dan diakhiri dengan domain yang valid.
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return res.status(400).send({ message: "Email tidak valid!" });
+  }
 
-    if (email && password) {
-        const user = await User.findOne({ email });
-        if (user) {
-            if (user.password === password) {
-                const token = jwt.sign(
-                    {
-                        email: user.email,
-                        password: user.password,
-                    },
-                    process.env.JWT_KEY,
-                    { expiresIn: "1h" }
-                );
-                user.updateOne({ api_key: token });
+  if (email && password) {
+    const user = await User.findOne({ email });
+    if (user) {
+      if (user.password === password) {
+        const name = user.name;
+        const phone = user.phone_number;
 
-                return res.status(200).json({
-                    body: {
-                        email,
-                        token,
-                    },
-                });
-            } else {
-                return res.status(400).send({ message: "Password salah!" });
-            }
-        } else {
-            return res.status(404).send({ message: "User tidak ditemukan!" });
-        }
+        const token = jwt.sign(
+          {
+            email: user.email,
+            password: user.password,
+          },
+          process.env.JWT_KEY,
+          { expiresIn: "1h" }
+        );
+        user.updateOne({ api_key: token });
+
+        return res.status(200).json({
+          body: {
+            name,
+            phone,
+            email,
+            token,
+          },
+        });
+      } else {
+        return res.status(400).send({ message: "Password salah!" });
+      }
     } else {
-        return res.status(400).send({ message: "Semua field wajib diisi!" });
+      return res.status(404).send({ message: "User tidak ditemukan!" });
     }
+  } else {
+    return res.status(400).send({ message: "Semua field wajib diisi!" });
+  }
 };
 
 const registerUser = async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    // Regex untuk memvalidasi format email
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-        return res.status(400).send({ message: "Email tidak valid!" });
+  // Regex untuk memvalidasi format email
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return res.status(400).send({ message: "Email tidak valid!" });
+  }
+
+  if (email && password) {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).send({ message: "Email sudah digunakan!" });
     }
 
-    if (email && password) {
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).send({ message: "Email sudah digunakan!" });
-        }
+    const newUser = new User({ email, password });
+    await newUser.save();
 
-        const newUser = new User({ email, password });
-        await newUser.save();
+    const token = jwt.sign(
+      {
+        email: newUser.email,
+        password: newUser.password,
+      },
+      process.env.JWT_KEY,
+      { expiresIn: "1h" }
+    );
+    newUser.updateOne({ api_key: token });
 
-        const token = jwt.sign(
-            {
-                email: newUser.email,
-                password: newUser.password,
-            },
-            process.env.JWT_KEY,
-            { expiresIn: "1h" }
-        );
-        newUser.updateOne({ api_key: token });
-
-        return res.status(201).json({
-            body: {
-                email: newUser.email,
-                token,
-            },
-        });
-    } else {
-        return res.status(400).send({ message: "Semua field wajib diisi!" });
-    }
+    return res.status(201).json({
+      body: {
+        email: newUser.email,
+        token,
+      },
+    });
+  } else {
+    return res.status(400).send({ message: "Semua field wajib diisi!" });
+  }
 };
 
 const getAllUsers = async (req, res) => {
--- src/models/User.js
+++ src/models/User.js
@@ -5,14 +5,22 @@
 const { default: mongoose } = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  phone_number: {
+    type: String,
+    required: true,
+  },
 });
 
 // Validasi Data
